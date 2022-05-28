f70300816d817968c743dae2d81b1fb4ad1b28b9
Gustavo Marcelo Penido Pereira <82380592+guhpenido@users.noreply.github.com>
2022-05-28 17:28:31 -0300

    Add files via upload

---
+++ calcula.js
@@ -0,0 +1,19 @@
+function calcula() {
+    let voltasInput = document.getElementById("voltas");
+    let distanciaInput = document.getElementById("distancia");
+    let paradasInput = document.getElementById("paradas");
+
+    let distanciaPercorrida = voltasInput.value * 2;
+    let paradasTotal = voltasInput.value / 2;
+
+    if (voltasInput.value < 0) {
+        alert("Digite algum valor válido!");
+    } else {
+        paradasInput.value = "Helena deu " + paradasTotal + " paradas.";
+        distanciaInput.value = "Helena percorreu " + distanciaPercorrida + " KM a cavalo.";
+    }
+};
+
+function reseta() {
+    window.location.replace("/index.html");
+}
